84f93466e5a32262715a9eff0ee213c84632bc50
Angie Dai <anqidai@umich.edu>
2025-10-26 13:50:51 -0400

    加了场景

---
+++ scenes/初见/character_body_2d.gd
@@ -0,0 +1,5 @@
+extends CharacterBody2D
+
+@export var speed := 60                # 移动速度
+@export var move_time_range := Vector2(1.0, 3.0)  # 移动
+@export var wait_time_range := Vector2(1.0, 2.5)  # 停下来等
